4ed9086b821d498cf10352080f1151bd7e8e946a
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-04-23 14:11:22 +0500

    TOGGLE state active and inactive

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@
     Базовый Xstate
     <p>Counter App</p>
   </h1>
-  <div><button>Increment</button> <button>Decrement</button></div>
-
-  <h2>Count: 0</h2>
   <div>
-    <button>Activate/Inactivate</button>
+    <button @click="send('INCREMENT')" :disabled="state.matches('inactive')">
+      Increment
+    </button>
+    <button @click="send('DECREMENT')" :disabled="state.matches('inactive')">
+      Decrement
+    </button>
+  </div>
+
+  <h2>Count: {{ state.context.count }}</h2>
+  <div>
+    <button @click="send('TOGGLE')">{{ toggleButtonLabel }}</button>
   </div>
   <h3>Current State: Active/Inactive</h3>
 </template>
@@ -15,15 +22,27 @@
 <script>
 import { useMachine } from "@xstate/vue";
 import counterMachine from "./counterMachine";
+import { computed } from "@vue/runtime-core";
 
 export default {
   name: "App",
   components: {},
   setup() {
     const { state, send } = useMachine(counterMachine);
+
+    const toggleButtonLabel = computed(() => {
+      const current = state.value;
+      if (current.matches("active")) {
+        return "Inactivate";
+      } else if (current.matches("inactive")) {
+        return "Activate";
+      }
+    });
+
     return {
       state,
       send,
+      toggleButtonLabel,
     };
   },
 };
--- src/counterMachine.js
+++ src/counterMachine.js
@@ -8,44 +8,42 @@ const decCount = assign({
   count: (ctx) => ctx.count - 1,
 });
 
-const counterMachine = createMachine(
-  {
-    id: "counter",
-    initial: "active",
-    context: {
-      count: 0,
-    },
-
-    states: {
-      active: {
-        on: {
-          TOGGLE: {
-            target: "inactive",
+const counterMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QGMD2BXAdgFzAJwDoBDZbASwDcwBiAFQHkBxRgGQFFFQAHVWM81Jk4gAHogC0AJgCMAZgIB2AKwAGWQE4V62dLUAOWQBoQAT0QA2aQUl6VBvUtmSFClTIC+742iy5CJcipqAEkAOQBhACU2AFk2UNphHj4BISRRRAAWFQJZZQUNFXMC7SUFYzMEcR1zAjd1c3VpZSUlcxUFT28MHHxiUkoaABE2KNj4xPTk-jJBYTEEWT0CaXVJJTXMzNUtNYqJSVlMgky18zy9NaW8rpAfXsIyTADBuiZWDineGbn0hfEtisVNJ1sC9OZTpkFJJMvsECDcgppJlzI08rJ2gVzJ4vCBMKgIHBhPc-P1AmAkt9UvMJDolIpJMVpAZZFooeY4eIQVZJMCjki9HpGU0lLcSX0ni8qJSUrM0qAFuZ6a1zucVCpMvy9qYJNzrHynK5BUqQWKen4ZT95RkqrINAymSy2QoOTrbZc6siiusGspMtIce4gA */
+  createMachine(
+    {
+      context: { count: 0 },
+      id: "counter",
+      initial: "active",
+      states: {
+        active: {
+          on: {
+            TOGGLE: {
+              target: "inactive",
+            },
+            INCREMENT: {
+              actions: "incCount",
+            },
+            DECREMENT: {
+              actions: "decCount",
+            },
           },
-          INCREMENT: {
-            actions: ["incCount"],
-          },
-          DECREMENT: {
-            actions: ["decCount"],
+        },
+        inactive: {
+          on: {
+            TOGGLE: {
+              target: "active",
+            },
           },
         },
       },
-
-      inactive: {
-        on: {
-          TOGGLE: {
-            target: "active",
-          },
-        },
+    },
+    {
+      actions: {
+        incCount,
+        decCount,
       },
-    },
-  },
-  {
-    actions: {
-      incCount,
-      decCount,
-    },
-  }
-);
+    }
+  );
 
 export default counterMachine;
